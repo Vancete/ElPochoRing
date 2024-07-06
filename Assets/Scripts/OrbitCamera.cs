32ed14180801021dc877cb56a376789d7c4b1a7d
Vancete <vancete07@gmail.com>
2024-07-06 21:11:38 +0200

    Initial project files and config

---
+++ Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,129 @@
+﻿// Copyright (C) Yunus Kara 2019-2024. All Rights Reserved.
+
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField]
+    Transform Target;
+    [SerializeField]
+    bool ShowGUI = true;
+    [SerializeField]
+    bool DriftMode = false;
+    [SerializeField]
+    float Spring = 0.0f;
+    [SerializeField]
+    float Distance = 7.5f;
+    [SerializeField]
+    float TargetYOffset = 1.0f;
+    [SerializeField]
+    float TiltAngle = 10.0f;
+
+    Camera mCamera;
+    Vector3 mDeltaDir = new Vector3(0, 0, -1);
+    Vector3 mLastPos;
+    float mRotationY = -50.0f;
+    float mSensivity = 200.0f;
+    float mRotationOffset = 0f;
+    float mCurrentTilt = 0f;
+    Rect mWindowRect = new Rect(Screen.width - 130, 5, 125, 170);
+    bool mFreeze = false;
+    bool mIsMobile = false;
+    public bool mOrbitMode = false;
+    public void setTarget(Transform t) { Target = t; }
+    public void setSpring(float s) { Spring = s; }
+    public void setDistance(float d) { Distance = d; }
+    public bool innerCamera = false;
+
+    private float innerCameraSpring = 25f;
+    private float innerCameraDistance = 0.01f;
+    private float innerCameraOffset = 0.75f;
+    private float innerCameraTilt = 0f;
+    private float outerCameraSpring = 2.5f;
+    private float outerCameraDistance = 6f;
+    private float outerCameraOffset = 1f;
+    private float outerCameraTilt = 12f;
+
+    void Start()
+    {
+        //QualitySettings.vSyncCount = 0;  // VSync must be disabled
+        //Application.targetFrameRate = 1000;
+        mIsMobile = Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer;
+        mCamera = GetComponent<Camera>();
+        if (mCamera == null) mCamera = Camera.main;
+        mLastPos = Target.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C) || Input.GetKeyDown(KeyCode.JoystickButton3))
+        {
+            Spring = innerCamera ? outerCameraSpring : innerCameraSpring;
+            Distance = innerCamera ? outerCameraDistance : innerCameraDistance;
+            TargetYOffset = innerCamera ? outerCameraOffset : innerCameraOffset;
+            TiltAngle = innerCamera ? outerCameraTilt : innerCameraTilt;
+            innerCamera = !innerCamera;
+        }
+
+        //distance to target
+        // Distance -= Input.GetAxis("Mouse ScrollWheel") * mSensivity * Time.deltaTime;
+        // mOrbitMode = Input.GetMouseButton(1);
+
+        //look back
+        mRotationOffset = 0f;
+        if (Input.GetKey(KeyCode.B))
+        {
+            mRotationOffset = 180f;
+        }
+
+        if (!mIsMobile && mOrbitMode)
+        {
+            TiltAngle -= Input.GetAxis("Mouse Y") * mSensivity * Time.deltaTime;
+            mRotationY += Input.GetAxis("Mouse X") * mSensivity * Time.deltaTime;
+            //clamp rotY
+            TiltAngle = Mathf.Clamp(TiltAngle, 0f, 80f);
+        }
+
+        float cspring = mOrbitMode ? 0f : Spring;
+
+        //position update
+        //also update rotation when spring is zero
+        if (Target != null)
+        {
+            Quaternion rot = Quaternion.Euler(mCurrentTilt, mRotationY + mRotationOffset, 0);
+            Vector3 targetPos = Target.position + TargetYOffset * Vector3.up;
+            Vector3 camPos = targetPos + Distance * (rot * Vector3.back);
+            if (cspring <= 0f) camPos = targetPos + Distance * (rot * mDeltaDir);
+            if (!mFreeze) mCamera.transform.position = camPos;
+            if (cspring <= 0f) mCamera.transform.rotation = Quaternion.LookRotation(targetPos - camPos);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float cspring = mOrbitMode ? 0f : Spring;
+        //rotation update when spring is non zero
+        if (Target != null)
+        {
+            //lerp towards current tilt angle
+            float tilt = Target.rotation.eulerAngles.x + TiltAngle;
+            if (cspring > 0f) mCurrentTilt = Mathf.LerpAngle(mCurrentTilt, tilt, cspring * Time.fixedDeltaTime);
+            else mCurrentTilt = TiltAngle;
+            //lerp towards current yaw angle
+            float targetAngle = Target.rotation.eulerAngles.y;
+            if (DriftMode)
+            {
+                Vector3 delta = Target.position - mLastPos;
+                float speed = (delta.magnitude / Time.fixedDeltaTime) * 3.6f;
+                if (speed > 5f) targetAngle = Quaternion.LookRotation(delta).eulerAngles.y;
+            }
+            mRotationY = Mathf.LerpAngle(mRotationY, targetAngle, cspring * Time.fixedDeltaTime);
+            Quaternion rot = Quaternion.Euler(mCurrentTilt, mRotationY + mRotationOffset, 0);
+            Vector3 targetPos = Target.position + TargetYOffset * Vector3.up;
+            Vector3 camPos = targetPos + Distance * (rot * Vector3.back);
+            if (mFreeze) camPos = mCamera.transform.position;
+            if (cspring > 0f || mFreeze) mCamera.transform.rotation = Quaternion.LookRotation(targetPos - camPos);
+            mLastPos = Target.position;
+        }
+    }
+}
